fcdd4da0828442ae1dd4a72d863741afce7b9a2b
Mårten Frisk <marten.frisk@chasacademy.se>
2020-04-29 00:12:32 +0200

    Updates

---
+++ precache-manifest.494850cafdf0e7fd0ee218cfbb96bb37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa554a6931599b3e29287babfb7ec78",
+    "url": "/dailyui03/index.html"
+  },
+  {
+    "revision": "66bcc6aaaa90f7b1791b",
+    "url": "/dailyui03/static/css/main.ddbb8ce0.chunk.css"
+  },
+  {
+    "revision": "bd6b2eec3c73edd5d490",
+    "url": "/dailyui03/static/js/2.2a4f25a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dailyui03/static/js/2.2a4f25a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bcc6aaaa90f7b1791b",
+    "url": "/dailyui03/static/js/main.21da9a2d.chunk.js"
+  },
+  {
+    "revision": "5535b925e26ace469421",
+    "url": "/dailyui03/static/js/runtime-main.31b0260c.js"
+  },
+  {
+    "revision": "c184412f9b5d2e7f3a2932fbb186653c",
+    "url": "/dailyui03/static/media/card1.c184412f.jpg"
+  },
+  {
+    "revision": "93b8a06cfa30dd7c30f7a9fb36f36946",
+    "url": "/dailyui03/static/media/card2.93b8a06c.jpg"
+  },
+  {
+    "revision": "e63181f102b92dd9542efb7f3132ea2a",
+    "url": "/dailyui03/static/media/pokal2.e63181f1.svg"
+  }
+]);
